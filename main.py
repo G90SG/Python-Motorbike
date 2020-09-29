ba79b5c56fe2f278f81f0e93f547512837410869
Gemma Dingwall <gemma9261@hotmail.com>
2020-09-29 16:09:30 +0000

    file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 print ("Motorbike Value")
 value = 2000
-print ("£",value)
+
+
 while value>1000:
   limited_float=round(value,2)
   print("£",limited_float)
@@ -12,4 +13,4 @@ while value>1000:
 else:
   print ("Value is less than £1000")
 
-#how do i stop it before the £956.59#
+
